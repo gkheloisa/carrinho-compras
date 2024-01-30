278ce9891f56c3aa84966101602ee2e52a7336e1
Heloisa Kalmus <gkheloisa@gmail.com>
2024-01-29 21:29:24 -0300

    change

--- js/app.js
+++ js/app.js
@@ -8,19 +8,28 @@ function adicionar() {
     let produtoValorUnitario = split[1];
     let quantidadeSelecionada = document.getElementById('quantidade').value;
     let preco = quantidadeSelecionada * produtoValorUnitario;
-    if (quantidadeSelecionada == '') {
-        alert('Selecionar quantidade desejada do produto.');
+
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+    if (isNaN(quantidadeSelecionada) || quantidadeSelecionada <= 0) {
+        alert('Insira uma quantidade desejada do produto válida.');
+        return;
     }
     
     let sessaoProdutos = document.getElementById('lista-produtos');
     sessaoProdutos.innerHTML = sessaoProdutos.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidadeSelecionada}x</span> ${produtoNome} <span class="texto-azul">R$${preco}</span>
     </section>`;
-    
+
     total = total + preco;
     valorTotal = document.getElementById('valor-total');
     valorTotal.innerHTML = `R$ ${total}`;
     document.getElementById('quantidade').value = '';
+
+    
 }
 
 function limpar() {
